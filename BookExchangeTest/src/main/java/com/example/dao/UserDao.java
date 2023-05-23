93bf8ae37239af6a941c7e636be9bb9d0b7d2902
Czj-czj <57036598+Czj-czj@users.noreply.github.com>
2023-05-23 10:58:48 +0800

    Delete BookExchangeTest directory

--- BookExchangeTest/src/main/java/com/example/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.dao;
-
-import com.example.entity.User;
-
-public interface UserDao {
-    User findByUsernameAndPassword(String username, String password);
-}
--- BookExchangeTest/src/main/java/com/example/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.service;
-
-import com.example.entity.User;
-
-public interface UserService {
-    User login(String username, String password);
-}
